75187c170727768a0c42913ebf65ffe681b18af1
TrungQuach <qntrung42@gmail.com>
2018-06-11 08:35:43 +0700

    use Utils

--- lof.py
+++ lof.py
@@ -8,6 +8,8 @@ from sklearn.neighbors import LocalOutlierFactor
 from matplotlib.font_manager import FontProperties
 from sklearn.metrics import roc_curve, auc, roc_auc_score, f1_score
 from sklearn.externals import joblib
+import utils
+
 
 # merage vm2012 log format data
 # return a [samples,features] array
@@ -21,6 +23,7 @@ def merageVm2012Data(expression):
     Accs = Accs.reshape(Accs.shape[0] * Accs.shape[1])
     return Accs
 
+
 # read vm2012 log format data
 # read acceleration, log export datetime, log data span, frequency
 def readVm2012Data(df):
@@ -29,16 +32,17 @@ def readVm2012Data(df):
 
     return accs
 
-def numericalSort(value):
 
+def numericalSort(value):
     numbers = re.compile(r'(\d+)')
     parts = numbers.split(value)
     parts[1::2] = map(int, parts[1::2])
 
     return parts
 
+
 def spliteAcc2fft(accs, splite_n, freq, MAX_FREQ=None):
-    freqs = np.array(np.fft.fftfreq(splite_n, d=1.0/ freq))
+    freqs = np.array(np.fft.fftfreq(splite_n, d=1.0 / freq))
     if MAX_FREQ == None:
         MAX_FREQ = np.max(freqs)
     indexes = np.where((freqs >= 0) & (freqs <= MAX_FREQ))[0]
@@ -53,7 +57,7 @@ def spliteAcc2fft(accs, splite_n, freq, MAX_FREQ=None):
         amplitudes = np.array(amplitudeSpectrum)[indexes]
         datas.append(amplitudes)
 
-        start+= splite_n
+        start += splite_n
     datas = np.array(datas)
     return datas
 
@@ -75,22 +79,22 @@ freq = 100000
 N = 1024 * 2
 use_data_test = True
 
-def _init_data(input, is_use_data_test=True, rate = 0.7):
+
+def _init_data(input, is_use_data_test=True, rate=0.7):
     if is_use_data_test:
         no_row_test_normal = (int)(input[0].shape[0] * rate)
-        datas_train = np.r_(input[0][:no_row_test_normal], input[1], input[2])
+        datas_train = np.r_[input[0][:no_row_test_normal], input[1], input[2]]
         datas_test = input[0][-no_row_test_normal:]
 
         ground_truth = np.zeros(datas_test.shape[0], dtype=int)
         ground_truth[-(input[1].shape[0] + input[2].shape[0]):] = 1
     else:
-        datas_train = np.r_(input)
+        datas_train = np.r_[input]
         ground_truth = np.zeros(datas_train.shape[0], dtype=int)
         ground_truth[-(input[1].shape[0] + input[2].shape[0]):] = 1
 
+    return (datas_train, ground_truth, datas_test)
 
-
-    return (datas_train,ground_truth, datas_test)
 
 if __name__ == "__main__":
     # set Japanese Font
@@ -103,60 +107,84 @@ if __name__ == "__main__":
         print("Bearing position 1 : ", accs_bearing1.size)
         accs_gear1 = merageVm2012Data(gear_position1_data_path + '/' + csv_file_regular_expression)
         print("Gear position 1 : ", accs_gear1.size)
-        accs_norma2 = merageVm2012Data(normal_position2_data_path + '/' + csv_file_regular_expression)
-        print("Normal position 2 : ", accs_norma2.size)
-        accs_bearing2 = merageVm2012Data(bearing_position2_data_path + '/' + csv_file_regular_expression)
-        print("Bearing position 2 : ", accs_bearing2.size)
-        accs_gear2 = merageVm2012Data(gear_position2_data_path + '/' + csv_file_regular_expression)
-        print("Gear position 2 : ", accs_gear2.size)
+        # accs_norma2 = merageVm2012Data(normal_position2_data_path + '/' + csv_file_regular_expression)
+        # print("Normal position 2 : ", accs_norma2.size)
+        # accs_bearing2 = merageVm2012Data(bearing_position2_data_path + '/' + csv_file_regular_expression)
+        # print("Bearing position 2 : ", accs_bearing2.size)
+        # accs_gear2 = merageVm2012Data(gear_position2_data_path + '/' + csv_file_regular_expression)
+        # print("Gear position 2 : ", accs_gear2.size)
         plt.figure()
     except Exception as e:
         print(e.__str__())
 
     # model for position 1
-    best_roc_auc = 0.0
-    for n in range(1 , 10):
-        N = n * 1024
-        normal1_datas = spliteAcc2fft(accs_normal1, N, freq)
-        bearing1_datas = spliteAcc2fft(accs_bearing1, N, freq)
-        gear1_datas = spliteAcc2fft(accs_gear1, N, freq)
+    best_test_score = -np.inf
+    # N = 1024 * 2
+    # n_sample_out = 200
 
-        datas_train,ground_truth, datas_test =  _init_data([normal1_datas, bearing1_datas, gear1_datas], is_use_data_test = use_data_test, rate = 0.8)
 
-        lof_model = create_lof_model(datas_train.shape[0]//3).fit(datas_train)
+    for n_neighbor in [60,120,200,250]:
+        print ("n_neightbor:", n_neighbor )
 
-        ground_truth = np.zeros(datas_train.shape[0], dtype=int)
-        ground_truth[-(bearing1_datas.shape[0] + gear1_datas.shape[0]):] = 1
+        for n in [1]:  # range(1, 5):
+            N = 1024 * 2 ** n
+            normal1_datas = spliteAcc2fft(accs_normal1, N, freq)
+            bearing1_datas = spliteAcc2fft(accs_bearing1, N, freq)
+            gear1_datas = spliteAcc2fft(accs_gear1, N, freq)
 
-        if use_data_test:
-            y_score = -lof_model._decision_function(datas_test)
-        else:
+            n_sample_out = (int)(np.min([normal1_datas.shape[0], bearing1_datas.shape[0], gear1_datas.shape[0]]) * 0.3)
+            normal_datas_in, normal_datas_out = normal1_datas[n_sample_out:], normal1_datas[:n_sample_out]
+            bearing_datas_in, bearing_datas_out = bearing1_datas[n_sample_out:], bearing1_datas[:n_sample_out]
+            gear_datas_in, gear_datas_out = gear1_datas[n_sample_out:], gear1_datas[:n_sample_out]
+
+            datas = np.r_[normal_datas_in, bearing_datas_in, gear_datas_in]
+            labels = np.r_[np.zeros(normal_datas_in.shape[0]),  # 0 for inlier, 1 for outlier
+                           np.ones(bearing_datas_in.shape[0]),
+                           np.ones(gear_datas_in.shape[0])]
+
+            train_datas, test_datas, train_labels, test_labels = utils.split_train_test(datas=datas, labels=labels,
+                                                                                        frac=0.8)
+
+            lof_model = create_lof_model(n_neighbor).fit(
+                train_datas)  # create_lof_model(train_datas.shape[0] // 3).fit(train_datas)
             y_score = -lof_model.negative_outlier_factor_
-        # Compute ROC curve and ROC area for each class
-        fpr, tpr, thresholds = roc_curve(ground_truth, y_score)
-        roc_auc = auc(fpr, tpr)
-        #compute f1_score
-        y_pred = np.zeros(datas_train.shape[0], dtype=int)
-        y_pred[lof_model.negative_outlier_factor_ <= lof_model.threshold_] = 1
-        f1 = f1_score(ground_truth, y_pred)
+            # Compute ROC curve and ROC area for each class
+            fpr, tpr, thresholds = roc_curve(train_labels, y_score)
+            roc_auc = auc(fpr, tpr)
 
-        #select best model with best roc_auc
-        if best_roc_auc < roc_auc:
-            best_roc_auc = roc_auc
-            best_model = lof_model
-        plt.plot(fpr, tpr, lw=2, label='N = %d (area = %0.2f) F1 = %0.2f' % (N, roc_auc, f1))
-    plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
-    plt.xlim([0.0, 1.0])
-    plt.ylim([0.0, 1.05])
-    plt.xlabel('False Positive Rate')
-    plt.ylabel('True Positive Rate')
-    plt.title('ROC for position 1')
-    plt.legend(loc="best")
+            y_score_test = -lof_model._decision_function(test_datas)
+            y_pred = np.zeros(test_labels.shape[0])
+            y_pred[y_score_test >= lof_model.threshold_] = 1
+            f1 = f1_score(test_labels, y_pred)
+            print('[Traing phase] N = %d, F1: %.3f' % (N, f1))
+
+            # select best model with best roc_auc
+            if f1 > best_test_score:
+                best_test_score = f1
+                best_model = lof_model
+
+        print('[--Train--] Best F1 : %.3f' % best_test_score)
+
 
     # # save best model to disk
     # filename = 'finalized_model_1.sav'
     # joblib.dump(best_model, filename)
 
+    print('[Test phase] START ')
+    out_test_datas = np.vstack([normal_datas_out, bearing_datas_out, gear_datas_out])
+    out_test_labels = np.hstack([np.zeros(normal_datas_out.shape[0]),  # 0 for inlier, 1 for outlier
+                                 np.ones(bearing_datas_out.shape[0]),
+                                 np.ones(gear_datas_out.shape[0])])
+    # y_score = -best_model.negative_outlier_factor_
+    y_score_test = -best_model._decision_function(out_test_datas)
+    fpr, tpr, thresholds = roc_curve(out_test_labels, y_score_test)
+    roc_auc = auc(fpr, tpr)
+
+
+    y_pred = np.zeros(out_test_labels.shape[0])
+    y_pred[y_score_test >= best_model.threshold_] = 1
+    f1 = f1_score(out_test_labels, y_pred)
+    print('[Test phase] roc_auc score: %.3f, f1 score: %.3f ' % (roc_auc, f1))
 
     # # model for position 2
     # plt.figure()
@@ -191,7 +219,7 @@ if __name__ == "__main__":
     # plt.ylabel('True Positive Rate')
     # plt.title('ROC for position 2')
     # plt.legend(loc="best")
-    plt.show(block=True)
+    # plt.show(block=True)
 
     # # save best model to disk
     # filename = 'finalized_model_2.sav'
